22e58886ba4c8e226e52d92e1991ba690be9046b
Mateusz Palyz <m.palyz@pilot.co>
2015-05-26 20:46:19 +0200

    add checkbox to hide completed tasks and counter of uncompleted and handle them

--- simple-todos.js
+++ simple-todos.js
@@ -3,7 +3,17 @@ Tasks = new Mongo.Collection("tasks");
 if (Meteor.isClient) {
   Template.body.helpers({
     tasks: function () {
-      return Tasks.find({}, {sort: {createdAt: -1}});
+      if (Session.get("hideCompleted")) {
+        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+      } else {
+        return Tasks.find({}, {sort: {createdAt: -1}});
+      }
+    },
+    hideCompleted: function () {
+      return Session.get("hideCompleted");
+    },
+    incompleteCount: function () {
+      return Tasks.find({checked: {$ne: true}}).count();
     }
   });
 
@@ -18,6 +28,10 @@ if (Meteor.isClient) {
 
       e.target.text.value = "";
       return false;
+    },
+
+    "change .hide-completed input": function (e) {
+      Session.set("hideCompleted", e.target.checked);
     }
   });
 
